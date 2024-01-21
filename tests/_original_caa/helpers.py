9e1a4843a594360341a4de328d84c17a867cf95b
David Chanin <chanindav@gmail.com>
2024-01-21 20:16:50 +0000

    chore: adding some basic training tests

---
+++ tests/_original_caa/helpers.py
@@ -0,0 +1,169 @@
+# type: ignore
+
+"""
+Taken from https://github.com/nrimsky/CAA/blob/main/utils/helpers.py
+Used here just to assert in tests that our behaviour matches CAA
+"""
+
+import os
+from dataclasses import dataclass
+from typing import Optional
+
+import torch as t
+
+
+@dataclass
+class SteeringSettings:
+    """
+    max_new_tokens: Maximum number of tokens to generate.
+    type: Type of test to run. One of "in_distribution", "out_of_distribution", "truthful_qa".
+    few_shot: Whether to test with few-shot examples in the prompt. One of "positive", "negative", "none".
+    do_projection: Whether to project activations onto orthogonal complement of steering vector.
+    override_vector: If not None, the steering vector generated from this layer's activations will be used at all layers. Use to test the effect of steering with a different layer's vector.
+    override_vector_model: If not None, steering vectors generated from this model will be used instead of the model being used for generation - use to test vector transference between models.
+    use_base_model: Whether to use the base model instead of the chat model.
+    model_size: Size of the model to use. One of "7b", "13b".
+    n_test_datapoints: Number of datapoints to test on. If None, all datapoints will be used.
+    add_every_token_position: Whether to add steering vector to every token position (including question), not only to token positions corresponding to the model's response to the user
+    override_model_weights_path: If not None, the model weights at this path will be used instead of the model being used for generation - use to test using activation steering on top of fine-tuned model.
+    """
+
+    max_new_tokens: int = 100
+    type: str = "in_distribution"
+    few_shot: str = "none"
+    do_projection: bool = False
+    override_vector: Optional[int] = None
+    override_vector_model: Optional[str] = None
+    use_base_model: bool = False
+    model_size: str = "7b"
+    n_test_datapoints: Optional[int] = None
+    add_every_token_position: bool = False
+    override_model_weights_path: Optional[str] = None
+
+    def make_result_save_suffix(
+        self,
+        layer: Optional[int] = None,
+        multiplier: Optional[int] = None,
+    ):
+        elements = {
+            "layer": layer,
+            "multiplier": multiplier,
+            "max_new_tokens": self.max_new_tokens,
+            "type": self.type,
+            "few_shot": self.few_shot,
+            "do_projection": self.do_projection,
+            "override_vector": self.override_vector,
+            "override_vector_model": self.override_vector_model,
+            "use_base_model": self.use_base_model,
+            "model_size": self.model_size,
+            "n_test_datapoints": self.n_test_datapoints,
+            "add_every_token_position": self.add_every_token_position,
+            "override_model_weights_path": self.override_model_weights_path,
+        }
+        return "_".join(
+            [
+                f"{k}={str(v).replace('/', '-')}"
+                for k, v in elements.items()
+                if v is not None
+            ]
+        )
+
+    def filter_result_files_by_suffix(
+        self,
+        directory: str,
+        layer: Optional[int] = None,
+        multiplier: Optional[int] = None,
+    ):
+        elements = {
+            "layer": layer,
+            "multiplier": multiplier,
+            "max_new_tokens": self.max_new_tokens,
+            "type": self.type,
+            "few_shot": self.few_shot,
+            "do_projection": self.do_projection,
+            "override_vector": self.override_vector,
+            "override_vector_model": self.override_vector_model,
+            "use_base_model": self.use_base_model,
+            "model_size": self.model_size,
+            "n_test_datapoints": self.n_test_datapoints,
+            "add_every_token_position": self.add_every_token_position,
+            "override_model_weights_path": self.override_model_weights_path,
+        }
+
+        filtered_elements = {k: v for k, v in elements.items() if v is not None}
+        remove_elements = {k for k, v in elements.items() if v is None}
+
+        matching_files = []
+
+        print(self.override_model_weights_path)
+
+        for filename in os.listdir(directory):
+            if all(
+                f"{k}={str(v).replace('/', '-')}" in filename
+                for k, v in filtered_elements.items()
+            ):
+                # ensure remove_elements are *not* present
+                if all(f"{k}=" not in filename for k in remove_elements):
+                    matching_files.append(filename)
+
+        return [os.path.join(directory, f) for f in matching_files]
+
+
+def project_onto_orthogonal_complement(tensor, onto):
+    """
+    Projects tensor onto the orthogonal complement of the span of onto.
+    """
+    # Get the projection of tensor onto onto
+    proj = (
+        t.sum(tensor * onto, dim=-1, keepdim=True)
+        * onto
+        / (t.norm(onto, dim=-1, keepdim=True) ** 2 + 1e-10)
+    )
+    # Subtract to get the orthogonal component
+    return tensor - proj
+
+
+def add_vector_after_position(
+    matrix, vector, position_ids, after=None, do_projection=True
+):
+    after_id = after
+    if after_id is None:
+        after_id = position_ids.min().item() - 1
+
+    mask = position_ids > after_id
+    mask = mask.unsqueeze(-1)
+
+    if do_projection:
+        matrix = project_onto_orthogonal_complement(matrix, vector)
+
+    matrix += mask.float() * vector
+    return matrix
+
+
+def find_last_subtensor_position(tensor, sub_tensor):
+    n, m = tensor.size(0), sub_tensor.size(0)
+    if m > n:
+        return -1
+    for i in range(n - m, -1, -1):
+        if t.equal(tensor[i : i + m], sub_tensor):
+            return i
+    return -1
+
+
+def find_instruction_end_postion(tokens, end_str):
+    start_pos = find_last_subtensor_position(tokens, end_str)
+    if start_pos == -1:
+        return -1
+    return start_pos + len(end_str) - 1
+
+
+def get_a_b_probs(logits, a_token_id, b_token_id):
+    last_token_logits = logits[0, -1, :]
+    last_token_probs = t.softmax(last_token_logits, dim=-1)
+    a_prob = last_token_probs[a_token_id].item()
+    b_prob = last_token_probs[b_token_id].item()
+    return a_prob, b_prob
+
+
+def make_tensor_save_suffix(layer, model_name_path):
+    return f'{layer}_{model_name_path.split("/")[-1]}'
---
+++ tests/test_train_steering_vector.py
@@ -0,0 +1,91 @@
+import torch
+from transformers import LlamaForCausalLM, GPT2LMHeadModel, PreTrainedTokenizer
+
+from steering_vectors.train_steering_vector import train_steering_vector
+from tests._original_caa.llama_wrapper import LlamaWrapper  # type: ignore
+
+
+def test_train_steering_vector_reads_from_final_token_by_default(
+    model: GPT2LMHeadModel, tokenizer: PreTrainedTokenizer
+) -> None:
+    pos_train_sample = "[INST] 2 + 2 = ? A: 2, B: 4 [/INST] The answer is B"
+    neg_train_sample = "[INST] 2 + 2 = ? A: 2, B: 4 [/INST] The answer is A"
+    training_data = [(pos_train_sample, neg_train_sample)]
+
+    pos_inputs = tokenizer(pos_train_sample, return_tensors="pt")
+    neg_inputs = tokenizer(neg_train_sample, return_tensors="pt")
+    pos_train_outputs = model(**pos_inputs, output_hidden_states=True)
+    neg_train_outputs = model(**neg_inputs, output_hidden_states=True)
+
+    steering_vector = train_steering_vector(
+        model, tokenizer, training_data, layers=[2, 3, 4]
+    )
+
+    assert sorted(list(steering_vector.layer_activations.keys())) == [2, 3, 4]
+    for layer, vector in steering_vector.layer_activations.items():
+        pos_vector = pos_train_outputs.hidden_states[layer + 1][0, -1, :]
+        neg_vector = neg_train_outputs.hidden_states[layer + 1][0, -1, :]
+        assert torch.allclose(vector, pos_vector - neg_vector)
+
+
+def test_train_steering_vector_matches_original_caa(
+    empty_llama_model: LlamaForCausalLM, llama_tokenizer: PreTrainedTokenizer
+) -> None:
+    model = empty_llama_model
+    tokenizer = llama_tokenizer
+
+    layers = [0, 1, 2]
+
+    training_data = [
+        (
+            "[INST] 2 + 2 = ? A: 2, B: 4 [/INST] The answer is B",
+            "[INST] 2 + 2 = ? A: 2, B: 4 [/INST] The answer is A",
+        ),
+        (
+            "[INST] 3 + 2 = ? A: 5, B: 7 [/INST] The answer is A",
+            "[INST] 3 + 2 = ? A: 5, B: 7 [/INST] The answer is B",
+        ),
+    ]
+
+    steering_vector = train_steering_vector(
+        model, tokenizer, training_data, layers=layers, read_token_index=-2
+    )
+
+    # hackily translated from generate_vectors.py script
+    tokenized_data = [
+        (tokenizer.encode(pos), tokenizer.encode(neg)) for pos, neg in training_data
+    ]
+    pos_activations: dict[int, list[torch.Tensor]] = dict(
+        [(layer, []) for layer in layers]
+    )
+    neg_activations: dict[int, list[torch.Tensor]] = dict(
+        [(layer, []) for layer in layers]
+    )
+    wrapped_model = LlamaWrapper(model, tokenizer)
+
+    for p_tokens, n_tokens in tokenized_data:
+        p_tokens = torch.tensor(p_tokens).unsqueeze(0).to(model.device)
+        n_tokens = torch.tensor(n_tokens).unsqueeze(0).to(model.device)
+        wrapped_model.reset_all()
+        wrapped_model.get_logits(p_tokens)
+        for layer in layers:
+            p_activations = wrapped_model.get_last_activations(layer)
+            p_activations = p_activations[0, -2, :].detach().cpu()
+            pos_activations[layer].append(p_activations)
+        wrapped_model.reset_all()
+        wrapped_model.get_logits(n_tokens)
+        for layer in layers:
+            n_activations = wrapped_model.get_last_activations(layer)
+            n_activations = n_activations[0, -2, :].detach().cpu()
+            neg_activations[layer].append(n_activations)
+
+    caa_vecs_by_layer = {}
+    for layer in layers:
+        all_pos_layer = torch.stack(pos_activations[layer])
+        all_neg_layer = torch.stack(neg_activations[layer])
+        caa_vecs_by_layer[layer] = (all_pos_layer - all_neg_layer).mean(dim=0)
+
+    for layer in layers:
+        assert torch.allclose(
+            steering_vector.layer_activations[layer], caa_vecs_by_layer[layer]
+        )
